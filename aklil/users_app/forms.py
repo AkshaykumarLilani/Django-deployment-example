4f5c9f8e26f2809c80ebae9f04cc091cb5bc28bf
Akshaykumar Lilani <aksbaylilani60@gmail.com>
2018-02-08 06:48:50 +0530

    removed aklil

--- aklil/users_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from users_app.models import Users
-
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = Users
-        fields = "__all__"
--- aklil/users_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from users_app import views
-
-urlpatterns=[
-    url(r'^$',views.sign_up_view, name='signup'),
-    url(r'^users/', views.users, name="users"),
-
-]
